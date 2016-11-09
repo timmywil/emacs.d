7f502e4d756ef005a471063922854596028a7884
Timmy Willison <4timmywil@gmail.com>
2016-11-09 13:09:00 -0500

    [shell-integration.el] add shell clear buffer function

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -9,3 +9,13 @@
    "AUTH0_ISSUER"
    "AUTH0_CLIENT_ID"
    "AUTH0_CLIENT_SECRET"))
+
+(defun eshell-clear-buffer ()
+  "Clear terminal"
+  (interactive)
+  (let ((comint-buffer-maximum-size 0))
+    (comint-truncate-buffer)))
+
+(add-hook 'shell-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "C-l") 'eshell-clear-buffer)))
